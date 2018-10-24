67bc19115929491f1d260827bacc12158ee29235
Carlota Fernández <cavefe@gmail.com>
2018-10-24 09:31:04 +0200

    Ejemplo usando switch para detectar numero clicado

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ container.addEventListener("click", onClick);
 
 function onClick(event) {
   if (event.target.tagName === "BUTTON") {
-      const operators= event.target.innerText;
-      const resultado= document.querySelector(".resultado");
-      resultado.innerText=operators;
+      const pulsado = event.target.innerText;
+      const resultado = document.querySelector(".resultado");
+      resultado.innerText = pulsado;
+
+      switch (pulsado) {
+        case '1':
+          console.log('El 1');
+          break;
+        case '2':
+        case '3':
+          console.log('El 2 y el 3');
+          break;
+        default:
+          console.log('Otro numero: ' + pulsado + '.');
+      }
   }
 }
 
